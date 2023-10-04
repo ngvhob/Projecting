4c9847d80a6d4b172fe852e3decdf58bdaf13548
Vaibhav <devesh@mawaimail.com>
2023-10-04 15:31:06 +0530

    [ADD] Blah

--- request.php
+++ request.php
@@ -1,13 +1,21 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailOrUsername = $_POST["email"];
     $password = $_POST["password"];
-
-    // You can perform any necessary processing, such as database queries, validation, etc., here
-
-    // For demonstration purposes, let's assume we're just echoing back the received data
-    echo "$emailOrUsername $password";
+    if (!isset($emailOrUsername) || !isset($password)) {
+        echo "Please provide valid input!";
+    } else {
+        $to = "recipient@example.com";
+        $subject = "Subject of the email";
+        $message = "Hello,\n\nYou have received a login attempt from: " . $emailOrUsername;
+        $headers = "From: your_email@example.com"; // Replace with your email address
+        $mailSent = mail($to, $subject, $message, $headers);
+        if ($mailSent) {
+            echo "Email sent successfully.";
+        } else {
+            echo "Error sending email.";
+        }
+    }
 } else {
-    echo "Invalid request!";
+    echo "Something went wrong.";
 }
